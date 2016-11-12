bac6fbd6e01ad9b3a6db40f962c0196309c5a981
Yuriy Plugatariov <yuriy.plugatariov@pipedrive.com>
2016-11-12 11:28:24 +0200

    Test commit to make sure that EY works

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello from Node Knockout 2016!')
-})
+  response.send('Test response to make sure that engineyard is working');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
